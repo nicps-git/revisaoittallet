49cf95d06d8e0d5f3d8b5c9dc0847a84c543570e
nicps-git <47723413+nicps-git@users.noreply.github.com>
2023-11-20 21:35:17 -0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,8 @@
+echo Instalado pacotes:
+sudo apt-get update
+sudo apt-get install traceroute -Y
+sudo apt-get install git
+echo ----------------------------------
+echo $USER
+date +"%Y-%m-%d%H:%M:%S"
+curl ifconfig.me
